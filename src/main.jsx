c4b5b5f4dd0e463407a69cd51a8f6a14fd7b3e3e
Delgorithm <delgorithm@gmail.com>
2023-08-14 18:45:26 +0200

    feature | New link for Register and Login pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from './App.jsx'
-import './index.css'
+import App from './App.jsx';
+import './index.css';
 import { createBrowserRouter, RouterProvider, BrowserRouter } from "react-router-dom";
-import ErrorPage from './routes/error-page.jsx'
-import Home from './pages/Home/Home.jsx'
-import Bordeaux from './pages/Bordeaux/Bordeaux.jsx'
-import Paris from './pages/Paris/Paris.jsx'
-import Auth from './pages/Auth/Auth.jsx';
+import ErrorPage from './routes/error-page.jsx';
+import Home from './pages/Home/Home.jsx';
+import Bordeaux from './pages/Bordeaux/Bordeaux.jsx';
+import Paris from './pages/Paris/Paris.jsx';
+
+import AuthLogin from './pages/Auth/AuthLogin.jsx';
+import AuthRegister from './pages/Auth/AuthRegister.jsx';
+
 import Lyon from './pages/Lyon/Lyon.jsx';
 import Toulouse from './pages/Toulouse/Toulouse.jsx';
 
@@ -40,8 +43,13 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />
   },
   {
-    path: "/Auth",
-    element: <Auth />,
+    path: "/AuthLogin",
+    element: <AuthLogin />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/AuthRegister",
+    element: <AuthRegister />,
     errorElement: <ErrorPage />
   }
 
